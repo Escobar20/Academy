49ac929a94c481e78e36ff68f74932b67cc65a78
Amber Macias <arussek05@gmail.com>
2018-08-18 21:00:31 -0500

    adding one endpoint

--- WeatherPage/src/app/Services/weather.service.ts
+++ WeatherPage/src/app/Services/weather.service.ts
@@ -1,13 +1,49 @@
-import { Injectable } from "@angular/core";
-import { HttpClient } from "../../../node_modules/@types/selenium-webdriver/http";
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams, HttpHandler, HttpHeaders } from '@angular/common/http';
+import { Observable, pipe } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http/src/response';
+import { map } from 'rxjs/operators';
+
 
 @Injectable()
-export class Weather {
+export class WeatherService {
 
-     readonly KEYWeather = "52014a5500cde66578177e9913b53a89";  //propiedad de clases
+    readonly KEYWeather = '52014a5500cde66578177e9913b53a89';  // propiedad de clases
+    public cityName = 'Monterrey,MX';
+    public atributos;
+    readonly Params: HttpParams = new HttpParams()
+      .set('APPID', this.KEYWeather)
+      .set('q', this.cityName);
 
-     constructor( private _http : HttpClient){
+     constructor( private _http: HttpClient) {
 
      }
+
+    getTime2(): Observable<any> {
+      return this._http.get<Array<any>>('https://api.openweathermap.org/data/2.5/forecast', { params : this.Params})
+        .pipe(
+          map( data => {
+              if (this.checkIfListExcist(data.list)) {
+                data.list = data.list.map(dataa => {
+                  dataa.main.tempC = dataa.main.temp - 273.15;
+                  dataa.main.temp_maxC = dataa.main.temp_max - 273.15;
+                  dataa.main.temp_minC = dataa.main.temp_min - 273.15;
+                  return dataa;
+                });
+              }
+              console.log(data.list);
+              return data;
+          })
+        );
+    } // End Observable
+
+    public handleError(err: HttpErrorResponse) {
+      console.log(err.message);
+      return Observable.throw(err.message);
+    }
+
+    private checkIfListExcist(Lista): boolean {
+      return Lista;
+    }
 
 }
--- WeatherPage/src/app/dashboard/dashboard.component.ts
+++ WeatherPage/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { WeatherService } from '../Services/weather.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  private atributos;
 
-  constructor() { }
+  constructor(private weather: WeatherService ) { }
 
   ngOnInit() {
+
+    if (!this.weather.atributos) {
+      this.weather.getTime2()
+      .subscribe((atributos) => {
+        this.weather.atributos = atributos;
+        // this.Products = products;
+        // this.Tra.productosSubject.next(products);
+        console.log(atributos);
+      },
+      (err) => {
+        this.weather.handleError(err);
+      });
+    } else {
+      this.atributos = this.weather.atributos;
+    }
   }
 
 }
