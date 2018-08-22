de84190886359abc5ec3eb17d384860e68c85404
Escobar20 <jhovana_ashlee@hotmail.com>
2018-08-22 10:38:12 -0700

    Implementing images and  RouterModule

--- WeatherPage/src/app/detail/detail.component.ts
+++ WeatherPage/src/app/detail/detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { WeatherService } from '../Services/weather.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-detail',
@@ -9,7 +10,19 @@ import { WeatherService } from '../Services/weather.service';
 export class DetailComponent implements OnInit {
   private atributos;
 
-  constructor(private weather: WeatherService) { }
+  constructor(private weather: WeatherService,private route: ActivatedRoute) { 
+
+    // this.route.params.subscribe(res => {
+    //   let id;
+    //   id = res['id'];
+    //   console.log('id', id);
+
+    //   this._todosService.getTodo(id).subscribe( todo => {
+    //     this.todo = todo;
+    //   });
+    // });
+
+  }
 
   ngOnInit() {
     if (!this.weather.atributos) {
